e279e6af068b6160d5682d727ee097e30dc92ed3
Kajetan Dobczyk <kajetan.dobczyk@gmail.com>
2020-04-11 20:04:53 +0200

    change bottom navigation to drawer

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,53 +1,28 @@
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
 
 import Dictionary from 'src/screens/Dictionary';
 import Home from 'src/screens/Home';
-import { color, hexToRGBA } from 'src/theme';
 
-import { Screen, RootTabParamList } from './interfaces';
-
-const tabsIcons: Record<Screen, { normal: string; focused: string }> = {
-  Home: {
-    normal: 'ios-square-outline',
-    focused: 'ios-square',
-  },
-  Dictionary: {
-    normal: 'ios-list',
-    focused: 'ios-list-box',
-  },
-};
-
-const Tab = createBottomTabNavigator<RootTabParamList>();
+const Drawer = createDrawerNavigator();
 
 const Navigation = () => (
   <NavigationContainer>
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => (
-          <Ionicons
-            name={tabsIcons[route.name][focused ? 'focused' : 'normal']}
-            size={size}
-            color={color}
-          />
-        ),
-      })}
-      tabBarOptions={{
-        activeBackgroundColor: hexToRGBA(color.green, 0.9),
-        inactiveBackgroundColor: color.green,
-        activeTintColor: color.white,
-        inactiveTintColor: color.white,
-      }}
-    >
-      <Tab.Screen name="Home" component={Home} options={{ title: 'Plansza' }} />
-      <Tab.Screen
+    <Drawer.Navigator initialRouteName="Board">
+      <Drawer.Screen
+        name="Board"
+        component={Home}
+        options={{
+          title: 'Plansza',
+        }}
+      />
+      <Drawer.Screen
         name="Dictionary"
         component={Dictionary}
         options={{ title: 'Dwuliterówki' }}
       />
-    </Tab.Navigator>
+    </Drawer.Navigator>
   </NavigationContainer>
 );
 
